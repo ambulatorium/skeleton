ceb3e5b7135d96d76fed1c315627a8ec589825eb
David H Sianturi <davidhasiholans@gmail.com>
2017-12-14 16:00:50 +0700

    Change navbar

--- resources/views/partials/people/tab.blade.php
+++ resources/views/partials/people/tab.blade.php
@@ -17,6 +17,20 @@
                 <div class="dropdown-menu">
                     <a class="dropdown-item" href="/settings/groups">Health Care Provider</a>
                     <a class="dropdown-item" href="/polyclinics">Polyclinics/Speciality</a>
+                    <div class="dropdown-divider"></div>
+                    @can('view-appointments')
+                        <a class="dropdown-item" href="/appointments">Appointments</a>
+                    @endcan
+                    @can('view-patients')
+                        <a class="dropdown-item" href="/patients">Patients</a>
+                    @endcan
+                    @can('view-doctors')
+                        <a href="/doctors" class="dropdown-item">Doctors</a>
+                    @endcan
+                    {{--  pending, useless feature  --}}
+                    {{--  @can('view-counters')
+                        <a class="dropdown-item" href="/counters">Counters</a>
+                    @endcan  --}}
                 </div>
             </li>
         @endcan
