a6ed974e9559c5bc18d5b2bf1aaed2a178d1fadd
David H. Sianturi <davidhasiholans@gmail.com>
2018-01-07 01:17:28 +0700

    Change redirect url

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/people';
 
     /**
      * Create a new controller instance.
