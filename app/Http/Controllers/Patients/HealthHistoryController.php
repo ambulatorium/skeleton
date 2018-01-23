756bd76c16540acdb3286b83e7851f3740d79096
David H. Sianturi <davidhasiholans@gmail.com>
2018-01-23 13:13:54 +0700

    fix

--- app/Http/Controllers/Patients/HealthHistoryController.php
+++ app/Http/Controllers/Patients/HealthHistoryController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Patients;
 
-use App\Models\Doctor\Schedule;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Patient\HealthHistory;
