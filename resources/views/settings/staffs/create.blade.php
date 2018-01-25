af53b29a638462b9ba2c2943e7852957d23187d4
David H. Sianturi <davidhasiholans@gmail.com>
2018-01-25 21:16:29 +0700

    Setting staff design and layout

--- resources/views/settings/staffs/create.blade.php
+++ resources/views/settings/staffs/create.blade.php
@@ -3,29 +3,31 @@
 @section('title', 'Invite Staffs')
 
 @section('content')
-    <main class="col-md-4 offset-md-4 mt-4 mb-3">
-        <h4 class="text-center">Invite Staff</h4>
-        <hr>
-        
-        <form action="/settings/staffs" method="POST" class="form-horizontal">
-            {{ csrf_field() }}
-
-            <div class="form-group">
-                <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="E-mail" autofocus required>
+    <main class="col-md-4 offset-md-4 my-3 p-3">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-text text-capitalize">invite staff</h5>
             </div>
+            <div class="card-body">
+                <form action="/settings/staffs" method="POST" class="form-horizontal">
+                    {{ csrf_field() }}
 
-            <div class="form-group form-check form-check-inline">
-                @foreach($roles as $role)
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="role" value="{{ $role->name }}"> {{ ucfirst($role->name) }}
-                    </label>
-                @endforeach
-            </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="E-mail" autofocus required>
+                    </div>
 
-            <div class="form-group">
-                <button class="btn btn-block btn-danger" type="submit">INVITE</button>
-                <a href="/settings/staffs" class="btn btn-block btn-secondary">CANCEL</a>
+                    @foreach($roles as $role)
+                        <div class="form-group custom-control custom-radio custom-control-inline">
+                            <input type="radio" id="userRole{{ $role->name }}" class="custom-control-input" name="role" value="{{ $role->name }}" required>
+                            <label class="custom-control-label" for="userRole{{ $role->name }}">{{ ucfirst($role->name) }}</label>
+                        </div>
+                    @endforeach
+
+                    <hr>
+                    <button class="btn btn-sm btn-danger" type="submit">INVITE</button>
+                    <a href="/settings/staffs" class="btn btn-sm btn-secondary">CANCEL</a>
+                </form>
             </div>
-        </form>
+        </div>
     </main>
 @endsection
--- resources/views/settings/staffs/index.blade.php
+++ resources/views/settings/staffs/index.blade.php
@@ -8,40 +8,35 @@
 @endsection
 
 @section('content')
-    <main class="col-md-8 offset-md-2 mt-3 mb-3">
-        <div class="card">
-            <div class="card-header">
-                <a href="/settings/staffs/create" class="btn btn-outline-danger btn-sm float-right">Invite staff</a>
-                <h4 class="card-text">Staff Management</h4>
-            </div>
-        
-            <div class="table-responsive">
-                <table class="table table-hover table-rq">
-                    <thead class="thead-rq">
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Roles</th>
-                            <th>Join Date</th>
-                            <th>Manage</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($users as $user)
-                            <tr>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->roles->implode('name', ' ') }}</td>
-                                <td>{{ $user->created_at->diffForHumans() }}</td>
-                                <td>
-                                    <a href="/settings/staffs/{{ $user->id }}/edit" class="btn btn-secondary btn-sm">edit</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-
+    <main class="col-md-8 offset-md-2 my-3 p-3">
+        <div class="table-responsive">
+            <table class="table table-hover table-rq">
+                <thead class="thead-rq">
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Roles</th>
+                        <th>Join Date</th>
+                        <th>Manage</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->roles->implode('name', ' ') }}</td>
+                        <td>{{ $user->created_at->diffForHumans() }}</td>
+                        <td>
+                            <a href="/settings/staffs/{{ $user->id }}/edit" class="btn btn-secondary btn-sm">edit</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
+
+        <a href="/settings/staffs/create" class="btn btn-sm btn-outline-secondary float-right">Invite</a>
+        
     </main>
 @endsection
