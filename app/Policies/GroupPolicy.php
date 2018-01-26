75ecb1db850ff74de8aa890ddb493b2158173c14
David H. Sianturi <davidhasiholans@gmail.com>
2018-01-26 13:33:42 +0700

    Show message after register from invitation

--- app/Policies/GroupPolicy.php
+++ app/Policies/GroupPolicy.php
@@ -26,7 +26,6 @@ class GroupPolicy
         if ($user->can('edit-group')) {
             return $group->id === $user->staff->group_id;
         }
-
     }
 
     /**
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,9 +7,22 @@
     <div id="app">
         <a href="/" class="text-center text-secondary"><h1>RELIQUI</h1></a>
 
+        @include('partials.master.errors')
+
+        <div id="flash-msg">
+            @include('flash::message')
+        </div>
+
         @yield('form')
     </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script>
+        $(function () {
+            // flash auto hide
+            $('#flash-msg .alert').not('.alert-danger, .alert-important').delay(3000).slideUp(500);
+        })
+    </script>
 </body>
 </html>
