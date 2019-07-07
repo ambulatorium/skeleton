4be40df5133eb817ca647cf2a6348d433aed758e
davidhsianturi <davidhasiholans@gmail.com>
2019-07-07 23:13:04 +0000

    Apply fixes from StyleCI

--- config/cache.php
+++ config/cache.php
@@ -98,6 +98,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'ambulatory'), '_') . '_cache'),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'ambulatory'), '_').'_cache'),
 
 ];
